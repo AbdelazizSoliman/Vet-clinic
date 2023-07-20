49f8b15f65243aa61072f191628fe6dfb4f48291
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-07-20 11:47:35 +0300

    Add and Update queries

--- queries.sql
+++ queries.sql
@@ -44,11 +44,12 @@ DELETE FROM animals
 WHERE date_of_birth  > '2022-01-01';
 --
 BEGIN
-SAVEPOINT first_save;
---
+SAVEPOINT sp1;
 
 UPDATE animals
 SET weight_kg  = weight_kg  * -1;
+ROLLBACK TO SAVEPOINT sp1;
+
 
 UPDATE animals
 SET weight_kg = weight_kg * -1
@@ -59,12 +60,14 @@ SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
 
-SELECT neutered, MIN(weight_kg ), MAX(weight_kg)
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight
 FROM animals
-GROUP BY neutered;
+GROUP BY species;
 
 SELECT species, AVG(escape_attempts) AS average_escape_attempts FROM animals
 WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
 GROUP BY species;
 
-
+ SELECT neutered, MAX(escape_attempts) AS max_escape_attempts
+FROM animals
+ GROUP BY neutered;
