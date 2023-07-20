2d5067653e03c36ea8f902a5539efb3a5eef6005
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-07-20 22:20:41 +0300

    Create vet table, and specializations,visits join tables

--- schema.sql
+++ schema.sql
@@ -32,3 +32,27 @@ ALTER TABLE animals
 ADD COLUMN owner_id INT,
 ADD CONSTRAINT fk_species_id
 FOREIGN KEY (owner_id) REFERENCES owners (id);
+
+CREATE TABLE vets (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INTEGER NOT NULL,
+    date_of_graduation DATE
+);
+
+CREATE TABLE specializations (
+  id SERIAL PRIMARY KEY,
+  vet_id INT,
+  species_id INT,
+  FOREIGN KEY (vet_id) REFERENCES vets(id),
+  FOREIGN KEY (species_id) REFERENCES species(id)
+);
+
+CREATE TABLE visits (
+  id SERIAL PRIMARY KEY,
+  vet_id INT,
+  animal_id INT,
+  visit_date DATE,
+  FOREIGN KEY (vet_id) REFERENCES vets(id),
+  FOREIGN KEY (animal_id) REFERENCES animals(id)
+);
