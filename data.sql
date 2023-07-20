b3d08733f3dc63a93f33f337bc32576718a4151f
AbdelazizSoliman <abdelaziz.soliman89@gmail.com>
2023-07-20 23:04:35 +0300

    Insert data into new tables

--- data.sql
+++ data.sql
@@ -37,3 +37,41 @@ UPDATE animals SET owner_id=2 WHERE name IN ('Pikachu', 'Gabumon');
 UPDATE animals SET owner_id=3 WHERE name IN ('Devimon','Plantmon');
 UPDATE animals SET owner_id=4 WHERE name IN ('Charmander','Squirtle','Blossom');
 UPDATE animals SET owner_id=5 WHERE name IN ('Angemon','Boarmon');
+
+INSERT INTO vets (name, age, date_of_graduation) 
+VALUES('William Tatcher', 45,'2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-01-08');
+
+
+INSERT INTO visits (animal_id, vet_id, visit_date)
+SELECT 
+    animals.id AS animal_id,
+    vets.id AS vet_id,
+    visit_dates.visit_date
+FROM 
+    (VALUES
+        ('Agumon', 'William Tatcher', '2020-05-24'),
+        ('Agumon', 'Stephanie Mendez', '2020-07-22'),
+        ('Gabumon', 'Jack Harkness', '2021-02-02'),
+        ('Pikachu', 'Maisy Smith', '2020-01-05'),
+        ('Pikachu', 'Maisy Smith', '2020-03-08'),
+        ('Pikachu', 'Maisy Smith', '2020-05-14'),
+        ('Devimon', 'Stephanie Mendez', '2021-05-04'),
+        ('Charmander', 'Jack Harkness', '2021-02-24'),
+        ('Plantmon', 'Maisy Smith', '2019-12-21'),
+        ('Plantmon', 'William Tatcher', '2020-08-10'),
+        ('Plantmon', 'Maisy Smith', '2021-04-07'),
+        ('Squirtle', 'Stephanie Mendez', '2019-09-29'),
+        ('Angemon', 'Jack Harkness', '2020-10-03'),
+        ('Angemon', 'Jack Harkness', '2020-11-04'),
+        ('Boarmon', 'Maisy Smith', '2019-01-24'),
+        ('Boarmon', 'Maisy Smith', '2019-05-15'),
+        ('Boarmon', 'Maisy Smith', '2020-02-27'),
+        ('Boarmon', 'Maisy Smith', '2020-08-03'),
+        ('Blossom', 'Stephanie Mendez', '2020-05-24'),
+        ('Blossom', 'William Tatcher', '2021-01-11')
+    ) AS visit_dates (animal_name, vet_name, visit_date)
+JOIN animals ON animals.name = visit_dates.animal_name
+JOIN vets ON vets.name = visit_dates.vet_name;
